c55f83d02b0b78b3ca87847aa2d3a10175e0299d
Matteo Depalo <matteo@depalo.me>
2020-04-22 16:42:04 +0100

    Update port for heroku
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000', 'https://focaccia-client.now.sh/'] });
-  await app.listen(3001);
+  app.enableCors({ origin: ['http://localhost:3000', 'https://focaccia-client.now.sh'] });
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
