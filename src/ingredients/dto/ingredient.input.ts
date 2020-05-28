f3146669611418ba16a628f6365e7439da6b711e
Matteo Depalo <matteo@depalo.me>
2020-05-28 09:30:28 +0100

    Fix ingredient input type and group types
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

--- src/ingredients/dto/ingredient.input.ts
+++ src/ingredients/dto/ingredient.input.ts
@@ -1,12 +1,13 @@
 import { InputType, Field, Float, Int } from "@nestjs/graphql"
+import { IngredientType, IngredientGroup } from "../ingredient.entity"
 
 @InputType()
 export class IngredientInput {
   @Field(_type => Int)
   id?: number
   name?: string
-  type!: string
-  group!: string
+  type!: IngredientType
+  group!: IngredientGroup
   @Field(_type => Float)
   weight!: number
 }
