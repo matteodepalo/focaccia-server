ebb037cfd7ea9fd07ffaaa297dc5a9df10f770fe
Matteo Depalo <matteo@depalo.me>
2022-06-28 15:54:56 +0100

    Fix now.sh domain

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   // This will be overridden by the graphql cors configuration for the /graphql route
-  app.enableCors({ origin: ['http://localhost:3000', 'https://focaccia-client.now.sh', 'https://focaccia.app'] })
+  app.enableCors({ origin: ['http://localhost:3000', 'https://focaccia-client.vercel.app', 'https://focaccia.app'] })
   if (configService.get('SENTRY_DSN')) {
     Sentry.init({ dsn: configService.get('SENTRY_DSN') })
     app.useGlobalInterceptors(new SentryInterceptor())
